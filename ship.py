d93d84cdc9feeca213c6d4cd0d27a4f88d460284
jonchavezhub123 <70290382+jonchavezhub123@users.noreply.github.com>
2024-05-08 22:00:27 -0500

    Update ship.py

--- ship.py
+++ ship.py
@@ -12,7 +12,7 @@ class Ship(Sprite):
         self.screen_rect = ai_game.screen.get_rect() 
 
         #Load the ship image and get its rect.
-        self.image = pygame.image.load('/Users/jhonchavez/Desktop/Python_Crash_Course3/alien_invasion/ship.bmp')
+        self.image = pygame.image.load('ship.bmp')
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom center of the screen
